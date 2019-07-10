d00c59797d3ddf42365f2d80185333eb42a6c0c5
KonstantinKshnyakin <50551609+KonstantinKshnyakin@users.noreply.github.com>
2019-07-10 16:34:45 +0300

    Create task1807.java

---
+++ src/com/javarush/test/Level №18/task1807.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task18.task1807;
+
+/* 
+Подсчет запятых
+
+Условие:
+
+С консоли считать имя файла.
+Посчитать в файле количество символов ',', количество вывести на консоль.
+Закрыть потоки.
+
+Подсказка:
+нужно сравнивать с ascii-кодом символа ','.
+
+
+Требования:
+
+1. Программа должна считывать имя файла с консоли.
+2. Для чтения из файла используй поток FileInputStream.
+3. В консоль должно выводится число запятых в считанном файле.
+4. Поток чтения из файла должен быть закрыт.
+*/
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = bufferedReader.readLine();
+        bufferedReader.close();
+        FileInputStream fileInputStream = new FileInputStream(fileName);
+
+        int count = 0;
+        while (fileInputStream.available() > 0){
+            int nextByte = fileInputStream.read();
+            if (nextByte == 44){
+                count++;
+            }
+        }
+        fileInputStream.close();
+        System.out.println(count);
+    }
+}
