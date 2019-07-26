7367043eec99fac3023d6849fbae8ced1de2d8da
KonstantinKshnyakin <50551609+KonstantinKshnyakin@users.noreply.github.com>
2019-07-26 21:11:00 +0300

    Update miniProject_task2312.java

--- src/com/javarush/test/Level №22/miniProject_task2312.java
+++ src/com/javarush/test/Level №22/miniProject_task2312.java
@@ -138,3 +138,98 @@ public class Room {
         }
     }
 }
+
+
+package com.javarush.task.task23.task2312;
+
+public class Mouse {
+    private int x;
+    private int y;
+
+    public Mouse(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
+
+
+package com.javarush.task.task23.task2312;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Snake {
+    private List<SnakeSection> sections;
+    private boolean isAlive;
+    private SnakeDirection direction;
+
+    public Snake() {
+
+    }
+
+    public Snake(int x, int y) {
+        SnakeSection snakeSection = new SnakeSection(x, y);
+        isAlive = true;
+        sections = new ArrayList<>();
+        sections.add(snakeSection);
+
+    }
+
+    public List<SnakeSection> getSections() {
+        return sections;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public SnakeDirection getDirection() {
+        return direction;
+    }
+
+    public void setDirection(SnakeDirection direction) {
+        this.direction = direction;
+    }
+
+    public int getX() {
+        return sections.get(0).getX();
+    }
+
+    public int getY() {
+        return sections.get(0).getY();
+
+    }
+
+    public void move() {
+
+    }
+}
+
+
+package com.javarush.task.task23.task2312;
+
+public class SnakeSection {
+    private int x;
+    private int y;
+
+    public SnakeSection(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
